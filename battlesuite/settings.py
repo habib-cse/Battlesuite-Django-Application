385fadbd9a04957b25e1fb9a9ab04b13b5fadb26
habib-cse <habib41juwel@gmail.com>
2020-11-08 15:36:10 +0600

    static files added again

--- battlesuite/settings.py
+++ battlesuite/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'r4bs$xkifrg18@ogb3n(dve05e6p2v+_kys0uovz2lw75lztt*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['battlesuite.herokuapp.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['battlesuite.herokuapp.com']
 
 
 # Application definition
